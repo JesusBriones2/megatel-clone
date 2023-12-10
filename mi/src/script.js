ea163d099c2295a5c9736136369662b344adb2bf
Jesus Briones <leojesusbriones2@gmail.com>
2023-12-09 22:27:35 -0500

    remastered

--- mi/src/script.js
+++ mi/src/script.js
@@ -1,5 +1,8 @@
+// Cierra el menu en vista mobile al presionar un enlace.
 const inputNav = document.querySelector('#nav__input')
 
 document.querySelector('.nav__links').addEventListener('click', ({target}) => {
   if (target.matches('.nav__link')) inputNav.checked = false
 })
+
+
---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+
+		colors: {
+			'white': '#ffffff',
+			'cyan-blue': '#0dcaf0',
+			'cyan-blue-hover': '#2990d4',
+			'black-opacity': '#00000055',
+			'dark-gray': '#212529',
+			'gray': '#50575c',
+			'yellow': '#ffca2c',
+			'yellow-hover': '#eabb30',
+			'navy-blue': '#0e385e',
+			'navy-blue-200': '#0f3353'
+		}
+	},
+	plugins: [],
+}
