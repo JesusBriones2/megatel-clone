b00a3fe29e0e6a7ecc69e097e47de6d8a74687ec
leonel <leojesusbriones2@gmail.com>
2024-02-25 23:09:43 -0500

    update

--- mi/src/script.js
+++
@@ -1,8 +0,0 @@
-// Cierra el menu en vista mobile al presionar un enlace.
-const inputNav = document.querySelector('#nav__input')
-
-document.querySelector('.nav__links').addEventListener('click', ({target}) => {
-  if (target.matches('.nav__link')) inputNav.checked = false
-})
-
-
