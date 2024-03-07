28fa7ab10b28a9e435a8446ead24c9db1031c8df
leonel <leojesusbriones2@gmail.com>
2024-03-07 16:28:28 -0500

    update project

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-
-		colors: {
-			"pale-blue": "#E0EEEC",
-			"mid-night-blue": "#00315D",
-			"slate-gray": "#6C757D",
-			"vivid-blue": "#0095FA",
-			"dark-blue": "#0E385E"
-		}
+		extend: {
+			colors: {
+				"pale-blue": "#E0EEEC",
+				"mid-night-blue": "#00315D",
+				"slate-gray": "#6C757D",
+				"vivid-blue": "#0095FA",
+				"dark-blue": "#0E385E"
+			},
+			fontFamily: {
+        'inter': ['"Inter"', ...defaultTheme.fontFamily.sans],
+      },
+		},
 	},
 	plugins: [],
 }
